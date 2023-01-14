23936fa88b35020cd1e5f800ede9778cd4480e2c
Joschka <jgross@cs.uni-saarland.de>
2023-01-14 11:25:33 +0100

    add distance/interaction transform

---
+++ kinodata/transform/add_distances.py
@@ -0,0 +1,43 @@
+from typing import Tuple
+import torch
+from torch import FloatTensor, LongTensor
+
+from torch_geometric.transforms import BaseTransform
+from torch_geometric.data import Data, HeteroData
+from torch_cluster import radius
+
+
+def interactions_and_distances(
+    pos1: FloatTensor, pos2: FloatTensor, r: float
+) -> Tuple[LongTensor, FloatTensor]:
+    y_ind, x_ind = radius(pos1, pos2, r)
+    dist = (pos1[x_ind] - pos2[y_ind]).pow(2).sum(dim=1).sqrt()
+    return torch.stack((x_ind, y_ind)), dist
+
+
+class AddDistancesAndInteractions(BaseTransform):
+    def __init__(self, radius: float) -> None:
+        super().__init__()
+        self.radius = radius
+
+    def __call__(self, data: Data | HeteroData):
+        if isinstance(data, HeteroData):
+            _, edge_types = data.metadata()
+            for nt_a, rel, nt_b in edge_types:
+                edge_index, dist = interactions_and_distances(
+                    data[nt_a].pos, data[nt_b].pos, self.radius
+                )
+                data[nt_a, rel, nt_b].edge_index = edge_index
+                data[nt_a, rel, nt_b].dist = dist
+                return data
+        else:
+            raise NotImplementedError
+            ...
+
+
+if __name__ == "__main__":
+
+    x = torch.randn(5, 2)
+    y = torch.randn(3, 2)
+    edge_index, dist = interactions_and_distances(x, y, 1.5)
+    print(dist)
