43472af9388cbc0b5f17be94853da2f1e0ceaf44
Joschka Groß <jgross@conduit.cs.uni-saarland.de>
2023-01-23 11:14:32 +0100

    add acuumulate grad batch config arg

--- train.py
+++ train.py
@@ -55,23 +55,25 @@ def train(config):
         auto_select_gpus=True,
         max_epochs=config.epochs,
         accelerator=config.accelerator,
+        accumulate_grad_batches=config.accumulate_grad_batches
     )
 
     trainer.fit(model, datamodule=data_module)
 
 
 default_config = dict(
-    batch_size=64,
-    num_mp_layers=3,
-    hidden_channels=64,
+    batch_size=16,
+    accumulate_grad_batches=4,
+    num_mp_layers=4,
+    hidden_channels=128,
     lr=3e-4,
     act="silu",
-    weight_decay=1e-4,
+    weight_decay=1e-5,
     interaction_radius=5.0,
     epochs=100,
-    num_workers=1,
+    num_workers=16,
     mp_type="rbf",
-    rbf_size=32,
+    rbf_size=64,
     accelerator="gpu",
 )
 
