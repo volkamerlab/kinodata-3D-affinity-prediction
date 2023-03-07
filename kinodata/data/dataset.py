7d02498ff817247ae2c9f791a4b0326d24ab6d84
Michael Backenköhler <m.backenkoehler@gmail.com>
2023-03-07 16:32:00 +0100

    read new dataset (sdf)

--- kinodata/data/dataset.py
+++ kinodata/data/dataset.py
@@ -9,7 +9,9 @@ import rdkit.Chem.AllChem as AllChem
 import requests
 import torch
 import torch.nn.functional as F
+import multiprocessing as mp
 from rdkit import RDLogger
+from rdkit.Chem import PandasTools
 from rdkit.Chem.rdchem import BondType as BT
 from rdkit.Chem.rdmolops import AddHs
 from torch import Tensor
@@ -34,38 +36,23 @@ class KinodataDocked(InMemoryDataset):
     def __init__(
         self,
         root: str = str(_DATA),
-        add_bond_info: bool = True,
         remove_hydrogen: bool = True,
         transform: Callable = None,
         pre_transform: Callable = None,
         pre_filter: Callable = FilterActivityType(["pIC50"]),
     ):
-        self.add_bond_info = add_bond_info
         self.remove_hydrogen = remove_hydrogen
-        if self.add_bond_info and not self.remove_hydrogen:
-            warn(
-                "Adding explicit and implicit hydrogen when using template bond info is not supported.\n \
-                Setting removeHs to True."
-            )
-            self.remove_hydrogen = True
 
         super().__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self) -> List[str]:
-        return ["data_clean.csv"]  # TODO add pdb and mol2 files as zip at some point
+        return ["kinodata_docked_filtered.sdf.gz"]
 
     @property
     def processed_file_names(self) -> List[str]:
-        fn = "kinodata_docked"
-        if (self.add_bond_info, self.remove_hydrogen) != (True, True):
-            poss_preposition = lambda b: "with" if b else "without"
-            fn = (
-                f"kinodata_docked_{poss_preposition(self.add_bond_info)}_bonds"
-                + f"_{poss_preposition(self.remove_hydrogen)}_hydrogen"
-            )
-        return [f"{fn}.pt"]
+        return "kinodata_docked.pt"
 
     @property
     def pocket_dir(self) -> Path:
@@ -77,43 +64,23 @@ class KinodataDocked(InMemoryDataset):
 
     def make_df_from_raw(self) -> pd.DataFrame:
         print("Reading data frame..")
-        df = pd.read_csv(self.raw_paths[0], index_col="ident")
-
-        df = df.drop_duplicates(
-            subset=[
-                "target_dictionary.uniprot_id",
-                "compound_structures.canonical_smiles",
-            ]
+        df = PandasTools.LoadSDF(
+            self.raw_paths[0],
+            smilesName="compound_structures.canonical_smiles",
+            molColName="molecule",
+            embedProps=True,
         )
-
-        print("Gathering paths to ligand pdb files..")
-        ligand_pdb_files = {
-            int(fp.stem.split("_")[0]): fp
-            for fp in (Path(self.raw_dir) / "pdbs" / "ligand").iterdir()
-        }
-        df["ligand_pdb_file"] = [
-            ligand_pdb_files[ident] if ident in ligand_pdb_files else None
-            for ident in df.index
-        ]
-
-        # sanity check
-        missing_ligands = df["ligand_pdb_file"].isna()
-        if missing_ligands.any():
-            print(df[missing_ligands].head())
-            raise RuntimeError
+        df.set_index("ID", inplace=True)
 
         print("Checking for missing pocket mol2 files...")
-        # for some reason pandas reads structure ID as a float
-        # we drop those
-        df = df[df["structure_ID"].notna()]
-        df["structure_ID"] = df["structure_ID"].astype(int)
+        df["similar.klifs_structure_id"] = df["similar.klifs_structure_id"].astype(int)
         # get pocket mol2 files
         if not self.pocket_dir.exists():
             self.pocket_dir.mkdir(parents=True)
 
-        pbar = tqdm(df.iterrows(), total=len(df))
-        for ident, row in pbar:
-            structure_id = row["structure_ID"]
+        struc_ids = df['similar.klifs_structure_id'].unique()
+        pbar = tqdm(struc_ids, total=len(struc_ids))
+        for structure_id in pbar:
             fp = self.pocket_dir / f"{structure_id}_pocket.mol2"
             if fp.exists():
                 continue
@@ -121,6 +88,7 @@ class KinodataDocked(InMemoryDataset):
                 "https://klifs.net/api/structure_get_pocket",
                 params={"structure_ID": structure_id},
             )
+            resp.raise_for_status()
             if resp.ok:
                 fp.write_bytes(resp.content)
 
@@ -128,7 +96,7 @@ class KinodataDocked(InMemoryDataset):
             int(fp.stem.split("_")[0]): fp for fp in (self.pocket_dir).iterdir()
         }
         df["pocket_mol2_file"] = [
-            pocket_mol2_files[row["structure_ID"]] for _, row in df.iterrows()
+            pocket_mol2_files[row["similar.klifs_structure_id"]] for _, row in df.iterrows()
         ]
 
         return df
@@ -182,43 +150,13 @@ class KinodataDocked(InMemoryDataset):
         data_list = []
         skipped: List[str] = []
         print("Creating PyG data objects..")
-        pbar = tqdm(df.iterrows(), total=len(df))
-        for i, (ident, row) in enumerate(pbar):
-            pbar.update(1)
-            pbar.set_description(f"Skipped: {len(skipped)}")
+        def process_idx(ident):
+            print('start', ident)
             data = HeteroData()
 
-            ligand = Chem.MolFromPDBFile(
-                str(row["ligand_pdb_file"]), removeHs=self.remove_hydrogen
-            )
-
-            if ligand is None:
-                skipped.append(str(ident))
-                continue
+            df.loc[ident]
+            ligand = row["molecule"]
             data = add_atoms(ligand, data, "ligand")
-
-            if self.add_bond_info:
-                try:
-                    template_smiles = row["compound_structures.canonical_smiles"].split(
-                        "."
-                    )
-                    template_smiles.sort(key=len)
-                    ligand_template = Chem.MolFromSmiles(template_smiles[-1])
-                    if not self.remove_hydrogen:
-                        ligand_template = AddHs(ligand_template)
-                    ligand = AllChem.AssignBondOrdersFromTemplate(
-                        ligand_template, ligand
-                    )
-                    data = add_bonds(ligand, data, "ligand")
-                except (Chem.rdchem.AtomValenceException, ValueError) as e:
-                    # Draw.MolToFile(ligand, f"images/{ident}_ligand.png")
-                    # Draw.MolToFile(
-                    #     ligand_template, f"images/{ident}_ligand_template.png"
-                    # )
-                    skipped.append(
-                        f"Unable to add bonds from template for entry ident={ident}: {e}"
-                    )
-                    continue
 
             pocket = Chem.rdmolfiles.MolFromMol2File(
                 str(row["pocket_mol2_file"]),
@@ -226,15 +164,19 @@ class KinodataDocked(InMemoryDataset):
                 sanitize=True,
             )
             if pocket is None:
-                skipped.append(str(ident))
-                continue
+                print('skipping', ident)
+                return None
             data = add_atoms(pocket, data, "pocket")
 
             data.y = torch.tensor(row["activities.standard_value"]).view(1)
             data.activity_type = row["activities.standard_type"]
             data.ident = ident
 
-            data_list.append(data)
+            print('stop', ident)
+            return data
+
+        with mp.Pool(os.cpu_count()) as pool:
+            data_list = pool.map(process_idx, list(df.index))
 
         if len(skipped) > 0:
             print(f"Skipped {len(skipped)} unprocessable entries.")
