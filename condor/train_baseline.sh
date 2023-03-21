d02a360c863d419b12321e739edcb1506a2a40f9
Joschka Groß <jgross@conduit.cs.uni-saarland.de>
2023-03-21 13:26:05 +0100

    train for each scaffold split

--- condor/train_baseline.sh
+++ condor/train_baseline.sh
@@ -1,7 +1,6 @@
 cd /home/jgross/kinodata-docked-rescore
 export WANDB_API_KEY=$(cat wandb_api_key)
-
 for ID in 7 11 13 17 19
 do	
-	python3 train_regressor.py --data_split "data/splits/scaffold/scaffold_split_$ID.csv"
+	python3 train_ligand_gnn_baseline.py --data_split "data/splits/scaffold/scaffold_split_$ID.csv"
 done
