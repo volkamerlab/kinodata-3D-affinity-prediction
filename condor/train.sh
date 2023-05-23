e54349d523c8dcb0ee5025cdaf14dfb7cef2d44e
Joschka Groß <jgross@conduit.cs.uni-saarland.de>
2023-05-23 10:00:48 +0200

    update condor train bash scripts

--- condor/train.sh
+++ condor/train.sh
@@ -1,7 +1,8 @@
 cd /home/jgross/kinodata-docked-rescore
 export WANDB_API_KEY=$(cat wandb_api_key)
-
-for ID in 7 11 13 17 19
+for ID in 0 1 2 3 4
 do	
-	python3 train_regressor.py --data_split "data/splits/scaffold/scaffold_split_$ID.csv"
+	python3 train_egnn.py --data_split "data/splits/pocket/seed_$ID.csv"
+	python3 train_egnn.py --data_split "data/splits/scaffold/seed_$ID.csv"
+	python3 train_egnn.py --data_split "data/splits/random/seed_$ID.csv"
 done
--- condor/train_dti.sh
+++ condor/train_dti.sh
@@ -2,7 +2,7 @@ cd /home/jgross/kinodata-docked-rescore
 export WANDB_API_KEY=$(cat wandb_api_key)
 for ID in 0 1 2 3 4
 do	
-	python3 train_dti_baseline.py --data_split "data/splits/random/seed_$ID.csv"
-	python3 train_dti_baseline.py --data_split "data/splits/scaffold/seed_$ID.csv"
 	python3 train_dti_baseline.py --data_split "data/splits/pocket/seed_$ID.csv"
+	python3 train_dti_baseline.py --data_split "data/splits/scaffold/seed_$ID.csv"
+	python3 train_dti_baseline.py --data_split "data/splits/random/seed_$ID.csv"
 done
