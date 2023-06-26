2c7cab622166bbe149aaef9db44cd72782cc2c55
Joschka Groß <jgross@conduit.cs.uni-saarland.de>
2023-06-26 09:54:32 +0200

    add condor script for sparse transforme

--- condor/train_template.sh
+++ condor/train_template.sh
@@ -3,5 +3,5 @@ cd /home/jgross/kinodata-docked-rescore
 export WANDB_API_KEY=$(cat wandb_api_key)
 for ID in 0 1 2 3 4
 do	
-	python3 scripts/$1.py --data_split "data/splits/$2/seed_$ID.csv" --model_type "rel-egnn"
+	python3 scripts/$1.py --data_split "data/splits/$2/seed_$ID.csv"
 done
