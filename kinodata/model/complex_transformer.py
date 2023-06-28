004e432494d6595af3796537d8a390cc1a7ec77e
Joschka Groß <jgross@conduit.cs.uni-saarland.de>
2023-06-28 13:08:17 +0200

    add dropout to decoder:

--- kinodata/model/complex_transformer.py
+++ kinodata/model/complex_transformer.py
@@ -8,6 +8,7 @@ from torch.nn import (
     Linear,
     Module,
     Parameter,
+    Dropout,
     BatchNorm1d,
 )
 from torch.nn.init import zeros_
@@ -100,7 +101,8 @@ class ComplexTransformer(RegressionModel):
         ln3: bool = True,
         graph_norm: bool = True,
         precomputed_distance: bool = False,
-        decoder_hidden_layers: int = 2,
+        decoder_hidden_layers: int = 1,
+        dropout: float = 0.1,
     ) -> None:
         super().__init__(config)
         self.act = resolve_act(act)
@@ -135,7 +137,7 @@ class ComplexTransformer(RegressionModel):
         self.aggr = SoftmaxAggregation(learn=True, channels=hidden_channels)
         self.out = Sequential(
             *(
-                [BatchNorm1d(hidden_channels)]
+                [Dropout(dropout), BatchNorm1d(hidden_channels)]
                 + [
                     FF(hidden_channels, hidden_channels, self.act)
                     for _ in range(decoder_hidden_layers)
