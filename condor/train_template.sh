eefb31c2099e364cd6dd2d46270d8ec6afdb146a
Joschka Groß <jgross@conduit.cs.uni-saarland.de>
2023-08-04 15:17:39 +0200

    condor scripts for full eval

--- condor/train_template.sh
+++ condor/train_template.sh
@@ -3,5 +3,5 @@ cd /home/jgross/kinodata-docked-rescore
 export WANDB_API_KEY=$(cat wandb_api_key)
 for ID in 0 1 2 3 4
 do	
-	python3 scripts/$1.py --data_split "data/splits/$2/seed_$ID.csv"
+	python3 scripts/$1.py --split_type $2 --split_index $ID --config $4.yaml --filter_rmsd_max_value $3
 done
