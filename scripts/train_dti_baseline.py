63f7d71487730e198d2d596fc3409bbc1d94499d
Joschka <jgross@cs.uni-saarland.de>
2023-08-07 15:03:59 +0200

    refactor dti baseline

--- scripts/train_dti_baseline.py
+++ scripts/train_dti_baseline.py
@@ -12,8 +12,8 @@ from kinodata.model.dti import (
     GlobalSumDecoder,
     ResidueTransformer,
     KissimTransformer,
-    LigandGINE,
 )
+from kinodata.model.shared.gine import LigandGINE
 from kinodata.data.featurization.residue import known_residues
 from kinodata.training import train
 
@@ -27,9 +27,11 @@ def make_dti_model(config):
     if config.residue_featurization == ResidueFeaturization.Onehot:
         ResidueModel = ResidueTransformer
         config["residue_size"] = len(known_residues) + 1
-    if config.residue_featurization == ResidueFeaturization.Kissim:
+    elif config.residue_featurization == ResidueFeaturization.Kissim:
         ResidueModel = KissimTransformer
         config["residue_size"] = 6
+    else:
+        raise ValueError(config.residue_featurization)
     return DTIModel(config, LigandGINE, ResidueModel, GlobalSumDecoder)
 
 
@@ -50,5 +52,7 @@ if __name__ == "__main__":
     for key, value in config.items():
         print(f"{key}: {value}")
 
-    wandb.init(config=config, project="kinodata-docked-rescore", tags=["dti", "less-features"])
+    wandb.init(
+        config=config, project="kinodata-docked-rescore", tags=["dti", "less-features"]
+    )
     train(config, fn_model=make_dti_model)
