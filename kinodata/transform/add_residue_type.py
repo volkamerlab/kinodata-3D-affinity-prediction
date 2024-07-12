4c2851850e7679cd089a7faebd5459deb58f6f52
Michael Backenköhler <m.backenkoehler@gmail.com>
2024-07-12 09:10:46 +0200

    allow for more residue types

--- kinodata/transform/add_residue_type.py
+++ kinodata/transform/add_residue_type.py
@@ -5,6 +5,7 @@ from torch_geometric.data import HeteroData
 
 from ..types import NodeType, RelationType
 
+NUM_RESIDUES = 23
 
 class AddResidueType(BaseTransform):
     def __init__(self):
@@ -14,11 +15,15 @@ class AddResidueType(BaseTransform):
         ligand_store = data[NodeType.Ligand]
         pocket_store = data[NodeType.Pocket]
 
-        lig_residue = one_hot(data[NodeType.Ligand].residue.to(torch.int64), num_classes=21)
+        lig_residue = data[NodeType.Ligand].residue.to(torch.int64)
+        assert torch.max(lig_residue) < NUM_RESIDUES, lig_residue
+        lig_residue = one_hot(lig_residue, num_classes=NUM_RESIDUES)
         data[NodeType.Ligand].x = torch.cat(
             [data[NodeType.Ligand].x, lig_residue], dim=1
         )
-        pocket_residue = one_hot(data[NodeType.Pocket].residue, num_classes=21)
+        pocket_residue = data[NodeType.Pocket].residue
+        assert torch.max(pocket_residue) < NUM_RESIDUES, pocket_residue
+        pocket_residue = one_hot(data[NodeType.Pocket].residue, num_classes=NUM_RESIDUES)
         data[NodeType.Pocket].x = torch.cat(
             [data[NodeType.Pocket].x, pocket_residue], dim=1
         )
