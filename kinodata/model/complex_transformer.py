8a580c6d73e2dc6b2c221e13d3e36312375c8ec8
Michael Backenköhler <m.backenkoehler@gmail.com>
2024-08-02 10:18:30 +0200

    CGNN: factor out message passing and aggregation

--- kinodata/model/complex_transformer.py
+++ kinodata/model/complex_transformer.py
@@ -215,7 +215,7 @@ class ComplexTransformer(RegressionModel):
             )
         )
 
-    def graph_representation(self, data: HeteroData):
+    def message_passing(self, data: HeteroData):
         node_store = data[NodeType.Complex]
         node_repr = self.act(
             self.atomic_num_embedding(node_store.z)
@@ -229,6 +229,11 @@ class ComplexTransformer(RegressionModel):
                 node_repr, edge_repr, edge_index
             )
             node_repr = norm(node_repr, node_store.batch)
+        return node_repr
+
+    def graph_representation(self, data: HeteroData):
+        node_store = data[NodeType.Complex]
+        node_repr = self.message_passing(data)
         graph_repr = self.aggr(node_repr, node_store.batch)
         return graph_repr
 
