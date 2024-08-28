38504f301b3ec57e4c53837cce4d3b883e240b9a
Joschka Groß <jgross@cs.uni-saarland.de>
2024-08-28 13:31:07 +0200

    add captum ig crocodoc

---
+++ scripts/crocodoc_ig.py
@@ -0,0 +1,113 @@
+from types import MethodType
+
+import captum
+import torch
+from torch import Tensor
+from torch_geometric.data import HeteroData
+from torch_geometric.loader import DataLoader
+from tqdm import tqdm
+
+from kinodata.data.data_module import create_dataset
+from kinodata.data.dataset import Filtered, KinodataDocked
+from kinodata.data.grouped_split import KinodataKFoldSplit
+from kinodata.model.complex_transformer import ComplexTransformer
+from kinodata.transform.filter_metadata import FilterDockingRMSD
+from kinodata.transform.to_complex_graph import TransformToComplexGraph
+from kinodata.types import NodeType
+
+from crocodoc_residues import load_model_from_checkpoint
+
+
+def prepare_data(rmsd_thresh, split_type, split_fold):
+    to_cplx = TransformToComplexGraph(remove_heterogeneous_representation=True)
+    rmsd_filter = FilterDockingRMSD(rmsd_thresh)
+    data_cls = Filtered(KinodataDocked(), rmsd_filter)
+    test_split = KinodataKFoldSplit(split_type, 5)
+    split = test_split.split(data_cls())[split_fold]
+    test_data = create_dataset(
+        data_cls,
+        dict(),
+        split.test_split,
+        None,
+    )
+    forbidden_seq = set(
+        [
+            "KALGKGLFSMVIRITLKVVGLRILNLPHLILEYCKAKDIIRFLQQKNFLLLINWGIR",
+            "LIGKGDSARLDYLVVGRLLQLVREP",
+            "LIIGKGDFGKVELSALKVVDIIRLILDYLVVGRLLQLVRE",
+            "NKMGEGGFGVVYKVAVKKLQFDQEIKVMAKCQENLVELLGFCLVYVYMPNGSLLDRLSCFLHENHHIHRDIKSANILLISDFGLA",
+            "_ALNVLDMSQKLYLLSSLDPYLLEMYSYLILEAPEGEIFNLLRQYLHSAMIIYRDLKPHNVLFIAA",
+        ]
+    )
+    return [
+        to_cplx(data) for data in test_data if data.pocket_sequence not in forbidden_seq
+    ]
+
+
+# this is far from pretty but it works
+def inject_partial_forward(model: ComplexTransformer) -> ComplexTransformer:
+    def compute_initial_embeds(self: ComplexTransformer, data: HeteroData):
+        node_store = data[NodeType.Complex]
+        node_repr = self.initial_embed_nodes(data)
+        if (batch := node_store.get("batch", None)) is None:
+            batch = torch.zeros(
+                node_repr.size(0), dtype=torch.long, device=node_repr.device
+            )
+            node_store["batch"] = batch
+        edge_index, edge_repr = self.initial_embed_edges(data)
+
+        return node_repr, edge_repr, edge_index, node_store.batch
+
+    def forward_initial_embeds(
+        self: ComplexTransformer,
+        node_embed: Tensor,
+        edge_embed: Tensor,
+        edge_index: Tensor,
+        batch: Tensor,
+    ):
+        node_embed = node_embed.squeeze()
+        for sparse_attention_block, norm in zip(
+            self.attention_blocks, self.norm_layers
+        ):
+            node_embed, edge_embed = sparse_attention_block(
+                node_embed, edge_embed, edge_index
+            )
+            node_embed = norm(node_embed, batch)
+
+        graph_repr = self.aggr(node_embed, batch)
+        return self.out(graph_repr)
+
+    model.compute_initial_embeds = MethodType(compute_initial_embeds, model)
+    model.forward_initial_embeds = MethodType(forward_initial_embeds, model)
+    return model
+
+
+def compute_attributions(model: ComplexTransformer, loader: DataLoader):
+    ig = captum.attr.IntegratedGradients(model.forward_initial_embeds)
+    attrs = []
+    deltas = []
+    for data in tqdm(loader[:3]):
+        with torch.no_grad():
+            node_embed, edge_embed, edge_index, batch = model.compute_initial_embeds(
+                data
+            )
+            node_embed.unsqueeze_(0)
+        attr, delta = ig.attribute(
+            node_embed,
+            additional_forward_args=(edge_embed, edge_index, batch),
+            return_convergence_delta=True,
+            internal_batch_size=1,
+        )
+        attrs.append(attr)
+        deltas.append(delta)
+    return attrs, deltas
+
+
+if __name__ == "__main__":
+    rmsd, split_type, fold = 2, "scaffold-k-fold", 0
+    model, train_config = load_model_from_checkpoint(rmsd, split_type, fold, "CGNN-3D")
+    model = inject_partial_forward(model)
+    model.eval()
+    data = prepare_data(rmsd, split_type, fold)
+    attrs, deltas = compute_attributions(model, data)
+    pass
