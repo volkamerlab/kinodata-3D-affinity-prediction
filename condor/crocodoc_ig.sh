45b42e2abfd8ebd3a79590c5a4d58e042d422405
Joschka Groß <jgross@cs.uni-saarland.de>
2024-08-28 13:43:20 +0200

    pass args to sh and py

--- condor/crocodoc_ig.sh
+++ condor/crocodoc_ig.sh
@@ -1,3 +1,3 @@
 cd ${HOME}/kinodata-3D-affinity-prediction
 export WANDB_API_KEY=$(cat wandb_api_key)
-python3 scripts/crocodoc_ig.py 
+python3 scripts/crocodoc_ig.py --rmsd $1 --split_type $2 --fold $3
