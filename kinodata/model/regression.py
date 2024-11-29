988c073b762beffa03dc47ddecf46471fb23459d
Michael Backenköhler <m.backenkoehler@gmail.com>
2024-11-29 17:41:33 +0100

    Revert "the silly way"
    This reverts commit 050e75aade1e8a85cf28b5c47c01459d87695a1c.

--- kinodata/model/regression.py
+++ kinodata/model/regression.py
@@ -85,11 +85,7 @@ class RegressionModel(pl.LightningModule):
 
     def training_step(self, batch, *args) -> Tensor:
         pred = self.forward(batch).view(-1, 1)
-        offset = torch.ones(3).to(pred.device) * 500
-        shift_ligand_position(batch, offset)
-        pred_unbound = self.forward(batch).view(-1, 1)
-        shift_ligand_position(batch, - offset)
-        loss = self.criterion(pred, batch.y.view(-1, 1)) + resolve_loss('mse')(pred_unbound, batch.y.view(-1, 1) * 0.)
+        loss = self.criterion(pred, batch.y.view(-1, 1))
         self.log("train/loss", loss, batch_size=pred.size(0), on_epoch=True)
         return loss
 
@@ -153,6 +149,3 @@ class RegressionModel(pl.LightningModule):
             test_predictions.add(table, "predictions")
             wandb.log_artifact(test_predictions)
             pass
-
-def shift_ligand_position(data: HeteroData, shift: Tensor):
-    data[NodeType.Complex].pos = pos + shift * (1.0 - data[NodeType.Complex].is_pocket_atom.astype(torch.float32))
