abea7852e722f8a7d696d5629439fefa9d92ba5d
Joschka Groß <jgross@cs.uni-saarland.de>
2024-12-17 13:43:25 +0100

    add new transforms to module

--- kinodata/transform/__init__.py
+++ kinodata/transform/__init__.py
@@ -6,3 +6,4 @@ from .add_distances import (
 from .perturb_position import PerturbAtomPositions
 from .filter_metadata import FilterDockingRMSD, MetadataFilter
 from .to_complex_graph import TransformToComplexGraph
+from .baseline_masking import MaskLigand, MaskPocket, MaskLigandPosition
