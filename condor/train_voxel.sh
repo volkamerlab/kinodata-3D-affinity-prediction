93157f2fae875fb1c6245e58414fb67e2bd02f56
Joschka Groß <jgross@cs.uni-saarland.de>
2025-01-20 15:08:42 +0100

    add voxel sub files

---
+++ condor/train_voxel.sh
@@ -0,0 +1,4 @@
+cd ${HOME}/kinodata-3D-affinity-prediction
+pip install --yes docktgrid
+export WANDB_API_KEY=$(cat wandb_api_key)
+python3 scripts/train_voxel_model.py --split_type $1 --fold $2 --hidden_channels $3
