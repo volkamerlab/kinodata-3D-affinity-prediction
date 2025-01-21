fb125b87b278818dd2c726917274ee7b5cffb6a0
Joschka Groß <jgross@cs.uni-saarland.de>
2025-01-21 14:36:28 +0100

    set mp start method to spawn to torch stops complaining hopefully

--- scripts/train_voxel_model.py
+++ scripts/train_voxel_model.py
@@ -15,6 +15,7 @@ from lightning.pytorch.loggers import WandbLogger
 from sklearn.model_selection import GroupKFold
 from torch.nn import BatchNorm3d, Conv3d, Identity, MaxPool3d, ReLU, Sequential
 from torch.optim import AdamW
+import torch
 from torch.utils.data import DataLoader
 from torchmetrics.regression import PearsonCorrCoef
 from tqdm import tqdm
@@ -23,6 +24,7 @@ from kinodata.data.data_split import Split
 from kinodata.data.grouped_split import _generator_to_list
 from kinodata.data.voxel.dataset import make_voxel_dataset_split
 
+torch.multiprocessing.set_start_method("spawn")
 DATA_DIR = Path(__file__).parent.parent / "data"
 
 
