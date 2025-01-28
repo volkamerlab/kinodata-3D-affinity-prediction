1bffe9967d0e192d8b22c37b1406a5e5d81770f6
Joschka Groß <jgross@cs.uni-saarland.de>
2025-01-28 13:27:19 +0100

    remove dep hacks

--- condor/train_voxel.sh
+++ condor/train_voxel.sh
@@ -1,6 +1,3 @@
 cd ${HOME}/kinodata-3D-affinity-prediction
-pip install lightning==2.5.0
 export WANDB_API_KEY=$(cat wandb_api_key)
-cd docktgrid && pip install .
-cd ..
 python3 scripts/train_voxel_model.py --split_type $1 --fold $2 --hidden_channels $3
