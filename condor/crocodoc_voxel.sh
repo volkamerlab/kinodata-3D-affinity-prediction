79a304adf023fbabc9a88bf13d698949ee9f0004
Joschka Groß <jgross@cs.uni-saarland.de>
2025-02-12 14:51:06 +0100

    crocodoc sub file fix

--- condor/crocodoc_voxel.sh
+++ condor/crocodoc_voxel.sh
@@ -1,4 +1,4 @@
 cd ${HOME}/kinodata-3D-affinity-prediction
 pip install resmo
 export WANDB_API_KEY=$(cat wandb_api_key)
-python3 scripts/doc_the_voxel.py --fold $1 --model-name $2 --reference_only $3
+python3 scripts/doc_the_voxel.py --fold $1 --model-name $2 $3
