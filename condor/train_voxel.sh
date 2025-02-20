d39eb3646d392f7c90054685ab1806e634369eb9
Joschka Groß <jgross@cs.uni-saarland.de>
2025-02-20 10:27:54 +0100

    fix wrong argument unpacking in sh script

--- condor/train_voxel.sh
+++ condor/train_voxel.sh
@@ -1,3 +1,3 @@
 cd ${HOME}/kinodata-3D-affinity-prediction
 export WANDB_API_KEY=$(cat wandb_api_key)
-python3 scripts/train_voxel_model.py --split_type $1 --fold $2 --hidden_channels $2 --batch_size $3 --acc_grad_batches $4 --kernel_sizes $5 --lr $6 --lr_decay $7 --random_rotation_augmentations $8
+python3 scripts/train_voxel_model.py --split_type $1 --fold $2 --hidden_channels $3 --batch_size $4 --acc_grad_batches $5 --kernel_sizes $6 --lr $7 --lr_decay $8 --random_rotation_augmentations $9
