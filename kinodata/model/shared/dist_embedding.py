c83c534ade2e675f45add60076f0b0256e107aa1
Joschka Groß <jgross@cs.uni-saarland.de>
2025-02-28 19:39:52 +0100

    add option for fixed means and std in gaussian embedding

--- kinodata/model/shared/dist_embedding.py
+++ kinodata/model/shared/dist_embedding.py
@@ -1,24 +1,26 @@
 import torch
 import numpy as np
-from torch import Tensor
+from torch import Tensor, jit
 from torch.nn import Parameter, Module
 
 
+@jit.script
 def gaussian(x, mean, std):
     return torch.exp(-0.5 * (((x - mean) / std) ** 2)) / ((2 * np.pi**0.5) * std)
 
 
 class GaussianDistEmbedding(Module):
-    def __init__(self, size: int, max_dist: float) -> None:
+    def __init__(self, size: int, max_dist: float, learnable: bool = True) -> None:
         super().__init__()
         self.size = size
+        self.learnable = learnable
         self.d_cut = Parameter(
             torch.tensor([max_dist], dtype=torch.float32), requires_grad=False
         )
 
         means, stds = self._initial_params()
-        self.means = Parameter(means)
-        self.stds = Parameter(stds)
+        self.means = Parameter(means, requires_grad=learnable)
+        self.stds = Parameter(stds, requires_grad=learnable)
 
     def _initial_params(self):
         means = torch.linspace(0, self.d_cut.item(), self.size)
