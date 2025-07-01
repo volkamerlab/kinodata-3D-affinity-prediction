670ecb5731d33947100c238340f622a817af2c48
joschka-gross <jgross@cs.uni-saarland.de>
2025-07-01 15:56:13 +0200

    hpc debugging hack

--- scripts/crocodoc_any_model.py
+++ scripts/crocodoc_any_model.py
@@ -250,6 +250,9 @@ def main(
         val=on_val,
         test=on_test,
     )
+
+    datasets = {key: dataset[:1000] for key, dataset in datasets.items()}
+
     if predict_outfile is not None:
         predict_outfile = Path(predict_outfile)
         if not predict_outfile.parent.exists():
